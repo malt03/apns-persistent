4741f3456614fb71734155cc23ce7919d3672ec7
malt03 <malt.koji@gmail.com>
2015-09-10 12:21:16 +0900

    feedback_clientもopenしないと使えないように

--- lib/apns/persistent/feedback_client.rb
+++ lib/apns/persistent/feedback_client.rb
@@ -1,31 +1,31 @@
 require 'forwardable'
-require 'json'
 
 module Apns
   module Persistent
     class FeedbackClient
+      extend Forwardable
+      def_delegators :@connection, :open, :close, :opened?, :closed?
+
       def initialize(certificate: , passphrase: nil, sandbox: true)
         cer = File.read(certificate)
         @connection = Connection.new(FeedbackClient.gateway_uri(sandbox), cer, passphrase)
       end
 
       def unregistered_devices
-        devices = []
+        raise 'please open' if closed?
 
-        @connection.open
+        devices = []
         while line = @connection.read(38)
           feedback = line.unpack('N1n1H140')
           timestamp = feedback[0]
           token = feedback[2].scan(/.{0,8}/).join(' ').strip
           devices << {token: token, timestamp: timestamp} if token && timestamp
         end
-        @connection.close
-
         devices
       end
 
       def unregistered_devices_token
-        unregistered_devices.collect{|device| device[:token]}
+        unregistered_devices.collect{ |device| device[:token] }
       end
 
       private
