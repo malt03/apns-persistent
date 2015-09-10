383beb7ae3fa455e65407667d146eb6a6831a2bf
malt03 <malt.koji@gmail.com>
2015-09-10 14:30:55 +0900

    余計なテスト削除

--- spec/apns/persistent_spec.rb
+++ spec/apns/persistent_spec.rb
@@ -4,8 +4,4 @@ describe Apns::Persistent do
   it 'has a version number' do
     expect(Apns::Persistent::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
